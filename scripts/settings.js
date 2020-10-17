50b5dfe9b4aa5a6425b60ec12ec418d84146524d
Panslothda <19254254+panslothda@users.noreply.github.com>
2020-10-17 11:47:34 +0200

    Update settings.js

---
+++ scripts/settings.js
@@ -0,0 +1,26 @@
+
+
+
+settings:
+  main:
+    fields:
+      - type: string
+        name: user
+        caption: Username
+        default: SBI-
+        required: true
+      - type: string
+        name: key
+        caption: Password
+        required: true
+        inputType: password
+      - type: string
+        name: swift-url
+        caption: Openstack Swift URL
+        default: SBI-
+        required: true
+      - type: string
+        name: user
+        caption: Swiss Backup username
+        default: SBI-
+        required: true
